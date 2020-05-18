5602c4d00d17eedafe691790abc06c3ecff8b1fe
Roger Cundiff <rogerleecundiff@gmail.com>
2020-05-18 13:11:54 -0500

    Switch Travis build branch to master

--- build.sh
+++ build.sh
@@ -34,7 +34,8 @@ git clone https://github.com/kappnav/apis.git
 COMMIT=$(git rev-parse HEAD)
 docker build --pull --build-arg VERSION=$VERSION --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg COMMIT=$COMMIT -t ${IMAGE} .
 
-if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH"  == "travisUpdates" ]; then
+# If this build is being done on an update to the master branch then tag the image as "dev" and push to docker hub kappnav org
+if [ "$TRAVIS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH"  == "master" ]; then
     echo $DOCKER_PWD | docker login docker.io -u $DOCKER_USER --password-stdin
     targetImage=docker.io/kappnav/inv:dev
     echo "Pushing Docker image $targetImage"
